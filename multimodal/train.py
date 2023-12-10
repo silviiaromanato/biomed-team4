2f07bb9e90d4ce1bbb9c4ce664b2085d0fcf48bb
AlexanderSternfeld <alex.sternfeld@live.nl>
2023-12-10 11:06:14 +0100

    fix

--- multimodal/train.py
+++ multimodal/train.py
@@ -143,6 +143,7 @@ def train(model, train_data, val_data, test_data,
         load_best_model_at_end=True,
         metric_for_best_model="loss",
         greater_is_better=False,
+        early_stopping_patience=3,
 
         # W&B logging
         report_to='wandb',
@@ -162,12 +163,6 @@ def train(model, train_data, val_data, test_data,
         eval_dataset=val_data,
         compute_metrics=compute_metrics,
         data_collator=train_data.collate_fn,
-        # do early stopping with patience of 3
-        metric_for_best_model='loss',
-        greater_is_better=False,
-        # early stopping
-        early_stopping_patience=3,
-        
 
     )
 
